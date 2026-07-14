343d6e66f89cfcdcf240326b68660f32227ca499
unknown <bhaktishinde0012@gmail.com>
2026-07-14 10:07:58 +0530

    Prepare backend for deployment

--- server/server.js
+++ server/server.js
@@ -114,15 +114,9 @@ message:"ERP Server Running"
 
 
 
-const PORT =
-process.env.PORT || 5000;
 
+const PORT = process.env.PORT || 5000;
 
-
-app.listen(PORT,()=>{
-
-console.log(
-`Server running on ${PORT}`
-);
-
+app.listen(PORT, () => {
+ console.log(`Server running on port ${PORT}`);
 });
